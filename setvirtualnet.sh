52dfed70691bf18f3729a5a9f40d73078384bddf
Erwin van Londen <erwin.vanlonden@hds.com>
2015-12-03 19:15:12 +1100

    Added hostinterfaces on separate bridge
    Signed-off-by: Erwin van Londen <erwin.vanlonden@hds.com>

--- setvirtualnet.sh
+++ setvirtualnet.sh
@@ -21,23 +21,34 @@
 echo "Do you want to create or delete the network  c/d"
 read crde
 
+
+
 case $crde in
 
     c)
-    echo "How many networks you want to create 1/9"
-    read netnum
-    if [[ $netnum > 9 ]]; then {
+    echo "How many networks you want to create 1-9 (3)"
+    read -e -i 3 -t 5 netnum
+    if [[ $netnum > 9 ]]; then
         echo "No more than 9 networks...."
         exit
-    }
     fi
 
-    echo "Number of interfaces per network 1-50"
-    read intnum
-    if [[ $intnum > 50 ]]; then {
-        echo "No more than 50 interfaces"
-    }
+
+    echo "Number of uplink interfaces per network 1-3 (2)"
+    read -e -i 2 -t 5 intnum
+    if [[ $intnum > 3 ]]; then
+        echo "No more than 5 uplink interfaces"
+        exit
     fi
+
+    echo "Number of host interfaces per network (2 to 4) (4)"
+    read -e -i 4 -t 5 hostnum
+    if [[ $hostnum > 4 || $hostnum < 2 ]]; then
+        echo "No more than 4 and less than 2 host interfaces per network segment"
+        exit
+    fi
+
+
 
     ### Create the network
     # Add the bridges
@@ -45,14 +56,25 @@ case $crde in
         ovs-vsctl -- --if-exists del-br br5$x""0
         ovs-vsctl add-br br5$x""0
         echo "Network segment br5"$x" created"
-    # Add the interfaces to each network
+        ovs-vsctl add-br br6$x""0
+        echo "Network segment br6"$x" created"
+    # Add the uplink interfaces to each network
         for ((y=1 ; y<=$intnum ; y++))
             {
-                ip tuntap add mode tap vnet5$x$y
-                ovs-vsctl -- add-port br5$x""0 vnet5$x$y
-                ip link set vnet5$x$y up
-                echo "Interface vnet5"$x$y" on bridge br5"$x"0 created"
+                ip tuntap add mode tap ulink5$x$y
+                ovs-vsctl -- add-port br5$x""0 ulink5$x$y
+                ip link set ulink5$x$y up
+                echo "Uplink interface ulink5"$x$y" on bridge br5"$x"0 created"
             }
+    # Add the host interfaces connected to each switch
+    # The first interface needs to be connected to the switch the ot
+            for ((z=1 ; z<=$hostnum ; z++ )) {
+                ip tuntap add mode tap hlink5$x$z
+                ovs-vsctl -- add-port br6$x""0 hlink5$x$z
+                ip link set hlink5$x$z up
+                echo "Host interface hlink5"$x$z" on bridge br6"$x"0 created"
+
+        }
         }
     ;;
 
@@ -60,11 +82,11 @@ case $crde in
     ### Delete the network
 
     #network segments
-    for x in `ovs-vsctl list-br | grep br5` ; do
+    for x in `ovs-vsctl list-br | grep -E "br[5-6]"` ; do
         ovs-vsctl -- del-br $x
     done
     #interfaces
-    for x in `ifconfig | grep -E "^vnet5" | awk -F: '{print $1}'` ; do
+    for x in `ifconfig | grep -E "^ulink5|^hlink5" | awk -F: '{print $1}'` ; do
         ip link del $x
     done
     ;;
